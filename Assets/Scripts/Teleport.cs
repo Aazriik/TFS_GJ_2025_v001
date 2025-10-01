8434e4975716b65ee9cee6b1af094e879b22e201
Joshua <jwarrilo@my.centennialcollege.ca>
2025-10-01 04:27:18 -0400

    Update Player Movement
    - Added another TP area
    - Added flashlight rotation with player movement
    - Added camera follow mechanic when required

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,17 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
     public GameObject portal;
-Artemis-WalkingAnim
+
     private GameObject player;
-
-
-    void Start()
-    {
-        player = GameObject.FindWithTag("Player");
 
     private GameManager gm;
 
@@ -20,18 +13,17 @@ Artemis-WalkingAnim
     private void Start()
     {
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-main
+
+        player = GameObject.FindWithTag("Player");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-Artemis-WalkingAnim
-            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+            player.transform.parent.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
             gm.Teleport(new Vector2(portal.transform.position.x, portal.transform.position.y),
                 currentRoom, portal.GetComponent<TeleportAddOn>().currentRoom);
-main
         }
     }
 }

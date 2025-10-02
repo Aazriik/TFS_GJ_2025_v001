857cc00c69dec4d33e2c0b9791dec66f48fe7e04
Joshua <jwarrilo@my.centennialcollege.ca>
2025-10-02 17:22:24 -0400

    Update

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour
+{
+    public Button startButton;
+
+    public Image faderScreen;
+
+    public Animator faderAnimator;
+
+    public Animator startButtonAnimator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(StartGame());
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator StartGame()
+    {
+        faderAnimator.Play("Fade");
+
+        startButtonAnimator.Play("StartButtonFade");
+
+        yield return new WaitForSeconds(3.5f);
+
+        Debug.Log("Sounds playing!");
+    }
+}

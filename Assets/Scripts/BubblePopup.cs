044a031a9026de73c79c8a6991a75ed531776775
Aazriik <dibartemis@gmail.com>
2025-10-02 16:49:31 -0400

    Added Candle Lights and moved to level 1

--- Assets/Scripts/BubblePopup.cs
+++ Assets/Scripts/BubblePopup.cs
@@ -14,7 +14,7 @@ public class BubblePopup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "PlayerDet")
         {
            StartCoroutine(BubbleToggle());
         }

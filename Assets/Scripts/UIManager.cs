fd11273546fedca5b58c5fc8bfd275dbaf42b3d9
Aazriik <dibartemis@gmail.com>
2025-10-05 16:35:13 -0400

    Flashlight Animations

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -167,6 +167,7 @@ public class UIManager : MonoBehaviour
             button.onClick.RemoveAllListeners();
 
             flashLight.SetActive(true);
+            playerScript.hasFlashlight = true;
         }
     }
 
